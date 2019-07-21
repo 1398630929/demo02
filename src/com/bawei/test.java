4fe0ba931ea826d964c60743756930bff87c99ba
1398630929 <1398630929@qq.com>
2019-07-21 19:12:48 +0800

    测试

--- src/com/bawei/test.java
+++ src/com/bawei/test.java
@@ -3,6 +3,6 @@ package com.bawei;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试一下");
-        System.out.println("测试");
+        System.out.println("测试三");
     }
 }
